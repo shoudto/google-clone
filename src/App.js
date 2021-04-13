b671a74bda25dd99b0c5e6327fd5c4590999837a
Oscar Shoudt <shoudto@gmail.com>
2021-04-12 22:08:28 -0400

    Making my first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // BEM
+    <div className="app">
+      <h1>This is working</h1>
+
+      {/* Home (the one with the search on) */}
+      <Home />
+      {/* Search page (The results page) */}
     </div>
   );
 }
