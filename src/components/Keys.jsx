ede4be23b8d4a92dabee900c21abdc30d1d84b12
Oscar Shoudt <shoudto@gmail.com>
2021-04-13 17:25:28 -0400

    Making Changes for Google API

---
+++ src/components/Keys.jsx
@@ -0,0 +1,3 @@
+export const API_KEY = "AIzaSyCBj4HvYuxPtQMSWnTHDqIEI1OCzqcQSCU";
+
+export default API_KEY;
---
+++ src/hooks/useGoogleSearch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function useGoogleSearch = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default useGoogleSearch
