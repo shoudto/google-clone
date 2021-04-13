9e6cc3b05fdce34462e0c697713efdf3738abebc
Oscar Shoudt <shoudto@gmail.com>
2021-04-13 17:34:34 -0400

    Adding async function to fetch google api

--- src/hooks/useGoogleSearch.jsx
+++ src/hooks/useGoogleSearch.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import API_KEY from '../components/Keys'
+
+const CONTEXT_KEY = "2a321cb6dc87a0da9";
 
 function useGoogleSearch = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async() => {
+            fetch(
+                `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+            )
+        }
+    }, [term])
 }
 
 export default useGoogleSearch
