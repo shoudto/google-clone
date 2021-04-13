ad8d9c91c49210a66f3cbb3d6dac35c03d44daba
Oscar Shoudt <shoudto@gmail.com>
2021-04-13 12:53:00 -0400

    setting up state input with the search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Search.css';
 import SearchIcon from '@material-ui/icons/Search'
 import MicIcon from '@material-ui/icons/Mic'
 import { Button } from '@material-ui/core'
 
 function SearchBar() {
+
+    // How to set a variable in in react
+    const [input, setInput] = useState('');
+
+    const search = e => {
+        e.preventDefault();
+    }
+
     return (
-        <div className="search">
+        <form className="search">
             <div className="search__bar">
                 <SearchIcon className="search__icon" />
-                <input />
+                <input value={input} onChange={e => setInput(e.target.value)} />
                 <MicIcon />
             </div>
 
             <div className="search__buttons">
-                <Button variant="outlined">Google Search</Button>
+                <Button type="submit" onClick={search} variant="outlined">Google Search</Button>
                 <Button variant="outlined">I'm Feeling Lucky</Button>
             </div>
-        </div>
+        </form>
     )
 }
 
