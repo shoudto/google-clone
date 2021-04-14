e89e27d203c1f72be8c6d0a59fe5e525faaf477f
Oscar Shoudt <shoudto@gmail.com>
2021-04-13 20:35:50 -0400

    Implementing Links, Title, and bio for Search Page

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -6,6 +6,12 @@ import Response from '../Response'
 import { Link } from 'react-router-dom'
 import SearchBar from '../components/SearchBar'
 import SearchIcon from '@material-ui/icons/Search'
+import DescriptionIcon from "@material-ui/icons/Description"
+import ImageIcon from "@material-ui/icons/Image"
+import LocalOfferIcon from "@material-ui/icons/LocalOffer"
+import RoomIcon from "@material-ui/icons/Room"
+import MoreVertIcon from "@material-ui/icons/MoreVert"
+
 
 
 function SearchPage() {
@@ -32,47 +38,72 @@ function SearchPage() {
                     <div className="searchPage__headerBody">
                         <SearchBar hideButtons />
                         <div className="searchPage__options">
+                            {/* Beginning of Icon List - Left */}
                             <div className="optionsLeft">
-                                <div className="searchPage__options">
+                                <div className="searchPage__option">
                                     <SearchIcon />
                                     <Link to="/all">All</Link>
                                 </div>
-                                <div className="searchPage__options">
+                                <div className="searchPage__option">
                                     <DescriptionIcon />
                                     <Link to="/news">News</Link>
                                 </div>
-                                <div className="searchPage__options">
+                                <div className="searchPage__option">
                                     <ImageIcon />
                                     <Link to="/images">Images</Link>
                                 </div>
-                                <div className="searchPage__options">
+                                <div className="searchPage__option">
                                     <LocalOfferIcon />
                                     <Link to="/shopping">shopping</Link>
                                 </div>
-                                <div className="searchPage__options">
+                                <div className="searchPage__option">
                                     <RoomIcon />
                                     <Link to="/maps">maps</Link>
                                 </div>
-                                <div className="searchPage__options">
+                                <div className="searchPage__option">
                                     <MoreVertIcon />
                                     <Link to="/more">more</Link>
                                 </div>
                             </div>
+                            {/* End of Icon List - Left */}
+
 
                             <div className="optionsRight">
-
+                                <div className="searchPage__option">
+                                    <Link to="/settings">Settings</Link>
+                                </div>
+                                <div className="searchPage__option">
+                                    <Link to="/tools">Tools</Link>
+                                </div>
                             </div>
                         </div>
                     </div>
+                </div>
 
+                {true && (
                     <div className="searchPage__results">
+                        <p className="resultCount">
+                            About {data?.searchInformation.formattedTotalResults} results ({data?.searchInformation.formattedSearchTime} seconds) for {term}
+                        </p>
 
+                        {data?.items.map(item => (
+                            <div className="searchPage__result">
+                                <a href={item.link}>
+                                    {item.displayLink} 
+                                </a>
+
+                                <a href={item.link} className="searchPage__result Title">
+                                    <h2>{item.title}</h2>
+                                </a>
+
+                                <p className="searchPage__result Snippet">
+                                    {item.snippet}
+                                </p>
+                            </div>
+                        ))}
                     </div>
-                </div>
-
-                <div className="searchPage__results">
-
-                </div>
+                )}
+                
             </div>
         </div>
     )
